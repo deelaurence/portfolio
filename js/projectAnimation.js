1ce1a8081c53c958217b5bbfd8c7fecb8ed02ec5
lorraine-quantum~ <quantumoffice010@gmail.com>
2023-01-02 15:02:07 +0100

    manageinco

--- js/projectAnimation.js
+++ js/projectAnimation.js
@@ -5,10 +5,11 @@ gsap.to(".project-card1", {
     scrollTrigger: {
         trigger: ".project-card1",
         start: "top 20%",
-        end: "=+340",
+        end: "bottom 20%",
         // markers: true,
         scrub: 1,
         pin: true,
+        // anticipatePin: 3,
         pinSpacing: false
     },
 });
